4448d5eeaec623a2d7d678a40575ccc2d55128d5
Eason_Xu <eason820720@163.com>
2016-01-19 15:51:24 +0800

    1-19

---
+++ SVG/svg-icon/icon-animation/svgicons-config.js
@@ -0,0 +1,14 @@
+var svgIconConfig = {
+	cross : {
+		url : 'cross.svg',
+		animation : [
+			{ 
+				el : 'polygon',
+				animProperties : { 
+					from : { val : '{"transform" : "r0 220 220"}' },
+					to : { val : '{"transform" : "r45 220 220"}' }
+				} 
+			}
+		]
+	}
+};
